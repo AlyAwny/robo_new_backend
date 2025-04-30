33fe452e46a596b3da8b8615d83449401a7cfd71
Aly <alyawnyy@gmail.com>
2025-04-30 14:55:52 +0300

    new new

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+//const http = require("http");
+//const app = require("./app");
+//const setupWebSocket = require("./config/websocket");
+ 
+//const PORT = process.env.PORT || 8000;
+//const server = http.createServer(app);
+ 
+// Attach WebSocket
+//setupWebSocket(server);
+ 
+//server.listen(PORT, () => {
+  //console.log(`🚀 Server is running on http://localhost:${PORT}`);
+//});
 const http = require("http");
 const app = require("./app");
 const setupWebSocket = require("./config/websocket");
  
-const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
- 
-// Attach WebSocket
 setupWebSocket(server);
  
-server.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
+module.exports = (req, res) => {
+  server.emit("request", req, res);
+};
