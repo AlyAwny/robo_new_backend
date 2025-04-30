adc1c7d5772c6a366112fff553731be0b71c6184
Aly <alyawnyy@gmail.com>
2025-04-30 15:50:04 +0300

    a

--- app.js
+++ app.js
@@ -6,12 +6,51 @@ const robotRoutes = require("./routes/robot.routes");
 const app = express();
  
 // CORS
-app.use(
+
+/*app.use(
   cors({
     origin: "https://robot-front.vercel.app",
     credentials: true,
   })
-);
+);*/
+app.use((req, res, next) => {
+  const allowedOrigins = ["https://robot-front.vercel.app"];
+  const origin = req.headers.origin;
+ 
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+ 
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Requested-With"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+ 
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+ 
+  next();
+});
+ 
+// app.use(
+//   cors({
+//     origin: "https://prometica.vercel.app",
+//     // origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//     allowedHeaders: ["Content-Type"],
+//     credentials: true,
+//   })
+// );
+ 
+app.options("*", cors());
+
  
  app.get("/", (req, res) => {
   res.status(200).json({
